d65588a7d7aaf2d3c8b69732769bee95c628116b
Marcos Vinicius Sombra <marcos.sombraaa@gmail.com>
2022-04-01 21:50:54 -0300

    aula 03

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,7 +1,8 @@
 from django import views
 from django.urls import path
-from .views import index
+from .views import index, receita
 
 urlpatterns = [
     path('', index, name='index'),
+    path('receita', receita, name='receita'),
 ]
